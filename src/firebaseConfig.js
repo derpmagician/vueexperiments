c783b293e7a182f80d31b11f709288f6174d6fc8
derpmagician <renzin@gmail.com>
2023-10-08 19:56:23 -0500

    probando config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,24 @@ import { getAuth } from "firebase/auth";
 // import { getFirestore } from 'firebase/firestore/lite'
 
 
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.VITE_FIREBASE_APP_ID
+// };
+
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyBEj_lvulwL921R6C-GKJN1eC4haiLIk0Q",
+    authDomain: "vue3-2c548.firebaseapp.com",
+    projectId: "vue3-2c548",
+    storageBucket: "vue3-2c548.appspot.com",
+    messagingSenderId: "975447172434",
+    appId: "1:975447172434:web:d7afa045c68a84b2df468b"
 };
+
 
 initializeApp(firebaseConfig);
 const db = getFirestore();
